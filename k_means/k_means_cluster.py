81c6f9562f7fe0c31a0f0fedcc6684083dc2eb4b
alok <alokuttam.shukla@gmail.com>
2019-10-12 23:34:46 +0700

    solutions in python 3 for remains lectures

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -39,7 +39,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset_changed.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
@@ -48,8 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 ="total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2,feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,13 +59,16 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2,f3 in finance_features:
+    plt.scatter( f1, f2,f3 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+est = KMeans(n_clusters=3)
+est.fit(finance_features)
+pred = est.predict(finance_features)
 
 
 
@@ -73,4 +77,29 @@ plt.show()
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print ("no predictions object named pred found, no clusters to plot")
+
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+salary = []
+exercised_stock_options = []
+for name in data_dict:
+    stock = data_dict[name]['exercised_stock_options']
+    sal = data_dict[name]['salary']
+    if not np.isnan(float(stock)):
+        exercised_stock_options.append(float(stock))
+    if not np.isnan(float(sal)):
+        salary.append(float(sal))
+
+#Feature rescaling
+scaler = MinMaxScaler()
+print ("Rescaled salary $200,000:", scaler.fit_transform([[float(min(salary))], [200000], [float(max(salary))]]))
+print ("Rescaled salary $200,000:", scaler.fit_transform([[float(min(exercised_stock_options))], [1000000], [float(max(exercised_stock_options))]]))
+
+print ("Minimum stock value:", min(exercised_stock_options))
+print ("Maximum stock value:", max(exercised_stock_options))
+
+print ("Minimum salary value:", min(salary))
+print ("Maximum salary value:", max(salary))
+
+    
--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,22 +44,30 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print (path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text=parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            stopwords = ["sara", "shackleton", "chris", "germani"]
+            for word in stopwords:
+                text=text.replace(word,"")
+            
 
             ### append the text to word_data
+            word_data.append(text)    
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name=="sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
-print "emails processed"
+print ("emails processed")
 from_sara.close()
 from_chris.close()
 
